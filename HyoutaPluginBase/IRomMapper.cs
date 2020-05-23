075eba1597b2771ec05fe2b2320f7c76e27fd75a
Admiral H. Curtiss <pikachu025@gmail.com>
2020-05-23 22:14:47 +0200

    Move IRomMapper interface to HyoutaPluginBase.

---
+++ HyoutaPluginBase/IRomMapper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HyoutaPluginBase {
+	public interface IRomMapper {
+		bool TryMapRamToRom(uint ramAddress, out uint value);
+		bool TryMapRomToRam(uint romAddress, out uint value);
+	}
+}
---
+++ HyoutaUtils/RomMapperUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HyoutaPluginBase;
+
+namespace HyoutaUtils {
+	public static class RomMapperUtils {
+		public static uint MapRamToRom(this IRomMapper mapper, uint ramAddress) {
+			uint v;
+			if (mapper.TryMapRamToRom(ramAddress, out v)) {
+				return v;
+			}
+			throw new Exception("Address not mappable.");
+		}
+
+		public static uint MapRomToRam(this IRomMapper mapper, uint romAddress) {
+			uint v;
+			if (mapper.TryMapRomToRam(romAddress, out v)) {
+				return v;
+			}
+			throw new Exception("Address not mappable.");
+
+		}
+	}
+}
