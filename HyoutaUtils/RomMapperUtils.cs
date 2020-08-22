7df86c0a5013018a7a6c741f9e1792de4d4b2d7d
Admiral H. Curtiss <pikachu025@gmail.com>
2020-08-22 19:05:17 +0200

    Add compatibilty wrapper for 32bit rom mapping.

--- HyoutaUtils/RomMapperUtils.cs
+++ HyoutaUtils/RomMapperUtils.cs
@@ -3,6 +3,22 @@ using HyoutaPluginBase;
 
 namespace HyoutaUtils {
 	public static class RomMapperUtils {
+		public static bool TryMapRamToRom(this IRomMapper mapper, uint ramAddress, out uint value) {
+			ulong i = ramAddress;
+			ulong o;
+			bool b = mapper.TryMapRamToRom(i, out o);
+			value = (uint)(o & 0xffffffffu);
+			return b;
+		}
+
+		public static bool TryMapRomToRam(this IRomMapper mapper, uint romAddress, out uint value) {
+			ulong i = romAddress;
+			ulong o;
+			bool b = mapper.TryMapRomToRam(i, out o);
+			value = (uint)(o & 0xffffffffu);
+			return b;
+		}
+
 		public static ulong MapRamToRom(this IRomMapper mapper, ulong ramAddress) {
 			ulong v;
 			if (mapper.TryMapRamToRom(ramAddress, out v)) {
@@ -17,7 +33,22 @@ namespace HyoutaUtils {
 				return v;
 			}
 			throw new Exception("Address not mappable.");
+		}
 
+		public static uint MapRamToRom(this IRomMapper mapper, uint ramAddress) {
+			uint v;
+			if (mapper.TryMapRamToRom(ramAddress, out v)) {
+				return v;
+			}
+			throw new Exception("Address not mappable.");
+		}
+
+		public static uint MapRomToRam(this IRomMapper mapper, uint romAddress) {
+			uint v;
+			if (mapper.TryMapRomToRam(romAddress, out v)) {
+				return v;
+			}
+			throw new Exception("Address not mappable.");
 		}
 	}
 }
