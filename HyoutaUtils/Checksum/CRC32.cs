370e41896f868f9b1e18b9fc0423bde10e53fc30
Admiral H. Curtiss <pikachu025@gmail.com>
2020-08-30 16:07:01 +0200

    Move CRC32 checksum calculation functionality from HyoutaTools.

---
+++ HyoutaUtils/Checksum/CRC32.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyoutaUtils.Checksum {
+	public static class CRC32 {
+		public static uint CalculateCRC32FromCurrentPosition(Stream s, long bytecount) {
+			uint crc32 = CRC32Algorithm.crc_init();
+			crc32 = CRC32Algorithm.crc_update(crc32, s, (ulong)bytecount);
+			crc32 = CRC32Algorithm.crc_finalize(crc32);
+			return crc32;
+		}
+	}
+}
